dfa54e6816b71b87485ed6afa127a0bf95117a05
PSU-Maica <202280193@psu.palawan.edu.ph>
2023-12-26 22:31:02 +0800

    modified admin.py

--- flashcard/admin.py
+++ flashcard/admin.py
@@ -2,31 +2,21 @@ from django.contrib import admin
 from .models import *
 
 
-# @admin.register(Card)
-class CardInline(admin.TabularInline):
+@admin.register(Card)
+class Card(admin.ModelAdmin):
     model = Card
-    list_display = ['front', 'back', 'created_at', 'updated_at']
+    list_display = ('front', 'back', 'created_at', 'updated_at')
+    
+
+@admin.register(Topic)
+class Topic(admin.ModelAdmin):
+    model = Topic
+    list_display = ('topic', 'subject', 'created_at', 'updated_at')
     extra = 1
     
 
-# @admin.register(Topic)
-class TopicInline(admin.TabularInline):
-    model = Topic
-    list_display = ['topic', 'flash_cards', 'created_at', 'updated_at']
-    extra = 1
-    inlines = [
-        CardInline,
-        ]
-
-# @admin.register(Subject)
+@admin.register(Subject)
 class SubjectAdmin(admin.ModelAdmin):
     model = Subject
-    list_display = ['subject', 'topics', 'created_at', 'updated_at']   
-    inlines = [
-        TopicInline,
-    ]
-
-
-admin.site.register(Subject)
-admin.site.register(Topic)
-admin.site.register(Card)
+    list_display = ('subject', 'created_at', 'updated_at')   
+    
