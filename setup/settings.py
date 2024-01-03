b1c200c2eb7e1163b17e60034402d04e97fc948b
PSU-Maica <202280193@psu.palawan.edu.ph>
2024-01-03 16:20:09 +0800

    modified menu, settings, and .gitignore; for deployment

--- setup/settings.py
+++ setup/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-y(nc8gar^3m!!6h(i3vjg53p_47-^h!nl*pou-4@ym*$cer)3(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
