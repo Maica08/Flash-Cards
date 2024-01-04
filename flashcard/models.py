557124f73747e104d49d086bc138c8f3c43cdd1e
PSU-Maica <202280193@psu.palawan.edu.ph>
2024-01-04 15:16:31 +0800

    modified models: set ForeignKey fields to be true

--- flashcard/models.py
+++ flashcard/models.py
@@ -6,30 +6,30 @@ from django.contrib.auth.models import User
 class BaseModel(models.Model):
     created_at = models.DateTimeField(auto_now=True, db_index=True)
     updated_at = models.DateTimeField(auto_now=True)
-    user = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, null=True, blank=False, on_delete=models.CASCADE)
     
     class Meta:
         abstract = True
 
 
 class Subject(BaseModel):
-    subject = models.CharField(max_length=100, null=True, blank=True)
+    subject = models.CharField(max_length=100, null=True, blank=False)
 
     def __str__(self):
         return self.subject
 
 
 class Topic(BaseModel):
-    topic = models.CharField(max_length=100, null=True, blank=True)
-    subject = models.ForeignKey(Subject, null=True, blank=True, on_delete=models.CASCADE)
+    topic = models.CharField(max_length=100, null=True, blank=False)
+    subject = models.ForeignKey(Subject, null=True, blank=False, on_delete=models.CASCADE)
     
     def __str__(self):
         return self.topic
 
 
 class Quiz(BaseModel):
-    name = models.CharField(max_length=255, null=True, blank=True)
-    topic = models.ForeignKey(Topic, null=True, blank=True, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255, null=True, blank=False)
+    topic = models.ForeignKey(Topic, null=True, blank=False, on_delete=models.CASCADE)
     
     
     class Meta:
@@ -42,24 +42,24 @@ class Quiz(BaseModel):
 class Card(BaseModel):
     front = models.CharField(max_length=255)
     back = models.TextField()
-    topic = models.ForeignKey(Topic, null=True, blank=True, on_delete=models.CASCADE)
-    flash_quiz = models.ManyToManyField(Quiz, blank=True)       
+    topic = models.ForeignKey(Topic, null=True, blank=False, on_delete=models.CASCADE)
+    flash_quiz = models.ManyToManyField(Quiz, blank=False)       
     
     def __str__(self):
         return self.front
 
 
 class Question(BaseModel):
-    quiz = models.ForeignKey(Quiz,null=True, blank=True, on_delete=models.CASCADE)
-    card = models.ForeignKey(Card,null=True, blank=True, on_delete=models.CASCADE)
+    quiz = models.ForeignKey(Quiz,null=True, blank=False, on_delete=models.CASCADE)
+    card = models.ForeignKey(Card,null=True, blank=False, on_delete=models.CASCADE)
     
     def __str__(self):
         return self.card.back
 
 
 class Choice(BaseModel):
-    question = models.ForeignKey(Question,null=True, blank=True, on_delete=models.CASCADE)
-    card = models.ForeignKey(Card,null=True, blank=True, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question,null=True, blank=False, on_delete=models.CASCADE)
+    card = models.ForeignKey(Card,null=True, blank=False, on_delete=models.CASCADE)
     is_correct = models.BooleanField(default=False)
     
     def __str__(self):
